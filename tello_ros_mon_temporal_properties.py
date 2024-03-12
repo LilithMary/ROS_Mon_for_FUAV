4b8bc80046279ef475d75d9807195f142763566b
LilithMary <68646445+LilithMary@users.noreply.github.com>
2024-03-12 15:46:33 +0000

    Add files via upload

--- tello_ros_mon_temporal_properties.py
+++ tello_ros_mon_temporal_properties.py
@@ -1,22 +1,19 @@
 import oracle
 
-# replace "reaction" with field name that carries 'reactRed'
-# replace "percentage" with field name that carries 'battery'
-# replace "landingMode" with field name that carries 'safetyLanding' and 'criticalLanding'
 '''
-- /detectRed	std_msgs/Int16		- Amount of red pixels the drone camera is seeing
-- /detectBlue	std_msgs/Int16		- Amount of blue pixels the drone camera is seeing
-- /battery	std_msgs/Int16		- Battery percentage of the drone
-- /cmd_tello	std_msgs/String		- Used to send commands to the tello, for example"move_forward;180"
+- /detectRed	std_msgs.msg.Int16		- Amount of red pixels the drone camera is seeing
+- /detectBlue	std_msgs.msg.Int16		- Amount of blue pixels the drone camera is seeing
+- /battery	std_msgs.msg.Int16		- Battery percentage of the drone
+- /drone1/cmd_vel	geometry_msgs/Twist
 '''
 
 
 
 pl = [
-'(once[100:101]({topic: "detectRed", detectRed: True}) -> {topic: "agentReact", reactRed: True} )',
-'({topic: "battery", battery: "Safety"} -> {topic: "agLand", landingMode: "Safety"})',
-'({topic: "battery", battery: "Critical"} -> {topic: "agLand", landingMode: "Critical"})',
-'({topic: "cmd_vel", forwardMotion: True})'
+"(once[100:101]({topic: 'detectRed', detectRed: 'True'}) -> {topic: 'agentReact', data: 'reactRed'} )",
+"({topic: 'battery', battery: 'Safety'} -> {topic: 'agLand', data: 'safetyLanding'})",
+"({topic: 'battery', battery: 'Critical'} -> {topic: 'agLand', data: 'criticalLanding'})"#,
+#'({topic: "drone1/cmd_vel", forwardMotion: True})'
 ]
 
 # property to verify
@@ -30,20 +27,17 @@ predicates = dict()
 # function to abstract a dictionary (obtained from Json message) into a list of predicates
 def abstract_message(message):
     print(message)
-
+    predicates = dict()
 
     predicates['topic'] = message['topic']
-
+    predicates['time'] = float(message['time'])
+    
     if message['topic'] == "detectRed":
-    	detectRed = int(message['detectRed'])
-    	predicates['detectRed'] = (detectRed > 200)
- 
-    elif message['topic'] == "agentReact":
-    	reaction = str(message['agentReact'])
-    	predicates['reactRed'] = (reaction == 'reactRed')
+    	detectRed = int(message['data'])
+    	predicates['detectRed'] = str((detectRed > 200))
     	
     elif message['topic'] == "battery":
-    	percentage = int(message['battery'])
+    	percentage = int(message['data'])
     	if percentage >= 20 and percentage <= 40: 
     		predicates['battery'] = 'Safety'
     	elif percentage < 20:
@@ -51,18 +45,9 @@ def abstract_message(message):
     	else:
     		predicates['battery'] = 'Unspecified'
     		
-    elif message['topic'] == "agLand":
-    	landingMode = str(message['agLand'])
-    	if landingMode == 'safetyLanding':
-    		predicates['landingMode'] = 'Safety'
-    	elif landingMode == 'criticalLanding':
-    		predicates['landingMode'] = 'Critical'
-    	else:
-    		predicates['landingMode'] = 'Unspecified'
-
-    elif message['topic'] == 'cmd_vel':
-    	linearY = float(message['linear']['y'])
-    	predicates['forwardMotion'] = (linearY >= 0)
+    #elif message['topic'] == 'drone1/cmd_vel':
+    #	linearY = float(message['linear']['y'])
+    #	predicates['forwardMotion'] = (linearY >= 0)
     	
     #print(predicates)
     return predicates
