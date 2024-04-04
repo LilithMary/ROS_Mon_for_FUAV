40a0993986323234636863dae55a78476f75c95c
LilithMary <68646445+LilithMary@users.noreply.github.com>
2024-04-04 17:49:40 +0100

    Update tello_ros_mon_temporal_property_2.py

--- tello_ros_mon_temporal_property_2.py
+++ tello_ros_mon_temporal_property_2.py
@@ -1,23 +1,7 @@
 import oracle
 
-'''
-- /detectRed	std_msgs.msg.Int16		- Amount of red pixels the drone camera is seeing
-- /detectBlue	std_msgs.msg.Int16		- Amount of blue pixels the drone camera is seeing
-- /battery	std_msgs.msg.Int16		- Battery percentage of the drone
-- /drone1/cmd_vel	geometry_msgs/Twist
-'''
-
-
-
-# pl = [
-# "(once[100:101]({topic: 'detectRed', detectRed: 'True'}) -> {topic: 'agentReact', data: 'reactRed'} )",
-# "({topic: 'battery', battery: 'Safety'} -> {topic: 'agLand', data: 'safetyLanding'})",
-# "({topic: 'battery', battery: 'Critical'} -> {topic: 'agLand', data: 'criticalLanding'})",
-# "(forall[t]. {topic: 'cmd_vel', time: *t} -> {topic: 'cmd_vel', forwardMotion: 'True', time: *t})"
-# ]
-
 # property to verify
-PROPERTY =  "({topic: 'battery', battery: 'Safety'} -> {topic: 'agLand', data: 'safetyLanding'})"
+PROPERTY =  "({topic: 'agLand', data: 'safetyLanding'} -> once{topic: 'battery', battery: 'Safety'})"
 
 # predicates used in the property (initialization for time 0)
 predicates = dict()
@@ -32,10 +16,9 @@ def abstract_message(message):
     predicates['topic'] = message['topic']
     predicates['time'] = str(message['time'])
     
-    #if message['topic'] == "detectRed":
-    #	detectRed = int(message['data'])
-    #	predicates['detectRed'] = str((detectRed > 200))
-    	
+    if message['topic'] in ["agLand", "battery"]:
+        predicates['data'] = str(message['data'])
+    
     if message['topic'] == "battery":
     	percentage = int(message['data'])
     	if percentage >= 20 and percentage <= 40: 
@@ -45,9 +28,5 @@ def abstract_message(message):
     	else:
     		predicates['battery'] = 'Unspecified'
     		
-    #elif message['topic'] == 'cmd_vel':
-    #	linearY = float(message.linear.y)
-    #	predicates['forwardMotion'] = str((linearY >= 0))
-    	
-    #print(predicates)
+   
     return predicates
