d8edfe25ffee128dae9df71f8cd57f25518e2ef2
LilithMary <68646445+LilithMary@users.noreply.github.com>
2024-04-04 17:57:47 +0100

    Add files via upload

---
+++ property_test2.py
@@ -0,0 +1,31 @@
+import oracle
+
+# property to verify
+PROPERTY = '''({topic: "agLand", data: "safetyLanding"} -> once{topic: "battery", battery: "Safety"})'''
+
+# predicates used in the property (initialization for time 0)
+predicates = dict()
+
+# in here we can add all the predicates we are interested in.. Of course, we also need to define how to translate Json messages to predicates.
+
+# function to abstract a dictionary (obtained from Json message) into a list of predicates
+def abstract_message(message):
+    print(message)
+    predicates = dict()
+
+    predicates['topic'] = message['topic']
+    predicates['time'] = str(message['time'])
+    predicates['data'] = str(message['data'])
+    
+    if message['topic'] == "battery":
+    	percentage = int(message['data'])
+    	if percentage >= 20 and percentage <= 40: 
+    		predicates['battery'] = 'Safety'
+    	elif percentage < 20:
+    		predicates['battery'] = 'Critical'
+    	else:
+    		predicates['battery'] = 'Unspecified'
+    print(predicates)		
+
+    #print(predicates)
+    return predicates
